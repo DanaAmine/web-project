0671429d804d21c11ab0a0d154d9b94a07eded23
Amine-Dz <112625154+DanaAmine@users.noreply.github.com>
2024-12-14 21:48:09 +0100

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -32,7 +32,7 @@ if (isset($_POST['submit'])) {
 <html lang="en">
   <head>
     <title>Login</title>
-    <link rel="stylesheet" href="CSS/loginpage.css">
+    <link rel="stylesheet" href="css/loginpage.css">
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
